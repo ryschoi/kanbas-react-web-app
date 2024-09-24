f803326ceaaa717f3f0b8757651039fc8564ae48
ryschoi <148027367+ryschoi@users.noreply.github.com>
2024-09-24 16:55:58 -0400

    Updated src

---
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -0,0 +1,148 @@
+export default function AssignmentEditor() {
+    return (
+      <div id="wd-assignments-editor">
+        <h2><label htmlFor="wd-name">Assignment Name</label></h2>
+        <input id="wd-name" defaultValue="A1 - ENV + HTML" /><br /><br />
+        <textarea id="wd-description">
+          The assignment is available online Submit a link to the landing page of your Web application running on Netlify. Tha landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
+        </textarea>
+        <br /> <br />
+
+        <table>
+          {/* Points */}
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-points">Points</label>
+            </td>
+            <td>
+              <input id="wd-points" value={100} />
+            </td>
+          </tr><br />
+
+          {/* Assignment group */}
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-group">Assignment Group</label>
+            </td>
+            <td>
+              <select name="wd-group" id="wd-group">
+                <option value="assignments">ASSIGNMENTS</option>
+                <option value="quizzes">QUIZZES</option>
+                <option value="exams">EXAMS</option>
+                <option value="projects">PROJECTS</option>
+              </select>
+            </td>
+          </tr><br />
+
+          {/* Display grade as */}
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-display-grade-as">Display grade as</label>
+            </td>
+            <td>
+              <select name="wd-display-grade-as" id="wd-display-grade-as">
+                <option value="percentage">Percentage</option>
+                <option value="letter">Letter</option>
+              </select>
+            </td>
+          </tr><br />
+
+          {/* Submission type */}
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-submission-type">Submission Type</label>
+            </td>
+            <td>
+              <select name="wd-submission-type" id="submission-type">
+                <option value="online">Online</option>
+                <option value="paper">Paper</option>
+              </select>
+              <br /><br />
+              {/* Online entry options */}
+              {/* <input type="checkbox" id="wd-text-entry" name="wd-text-entry" value="text entry">
+              <label for="wd-text-entry"> Text Entry</label><br />
+              
+              <input type="checkbox" id="wd-website-url" name="wd-website-url" value="website url">
+              <label for="wd-website-url"> Website URL</label> <br />
+              
+              <input type="checkbox" id="wd-media-recordings" name="wd-media-recordings" value="media recordings">
+              <label for="wd-media-recordings"> Media Recordings</label><br />
+              
+              <input type="checkbox" id="wd-student-annotation" name="wd-student-annotation" value="student annotation">
+              <label for="wd-student-annotation"> Student Annotation</label><br />
+              
+              <input type="checkbox" id="wd-file-upload" name="wd-file-upload" value="file upload">
+              <label for="wd-file-upload"> File Upload</label><br /> */}
+            </td>
+          </tr>
+          
+          {/* Online entry options */}
+          <tr>
+            <td align="right" valign="top"></td>
+            <td>
+            Online entry options
+            <div>
+              <input type="checkbox" id="wd-text-entry" name="wd-text-entry" value="text entry"/>
+              <label htmlFor="wd-text-entry"> Text Entry</label><br />
+            </div>
+            <div>
+              <input type="checkbox" id="wd-website-url" name="wd-website-url" value="website url"/>
+              <label htmlFor="wd-website-url"> Website URL</label> <br />
+            </div>
+            <div>
+              <input type="checkbox" id="wd-media-recordings" name="wd-media-recordings" value="media recordings"/>
+              <label htmlFor="wd-media-recordings"> Media Recordings</label><br />
+            </div>
+            <div>
+              <input type="checkbox" id="wd-student-annotation" name="wd-student-annotation" value="student annotation"/>
+              <label htmlFor="wd-student-annotation"> Student Annotation</label><br />
+            </div>  
+            <div>
+              <input type="checkbox" id="wd-file-upload" name="wd-file-upload" value="file upload"/>
+              <label htmlFor="wd-file-upload"> File Upload</label><br />
+            </div>
+            </td>
+          </tr><br />
+
+          {/* Assign*/}
+          <tr>
+            <td align="right" valign="top">
+            <label htmlFor="wd-assign-to">Assign</label>
+            </td>
+            <td>
+              Assign to<br />
+              <input type="text" id="wd-assign-to" value={"Everyone"} />
+              <br /><br />
+              Due<br />
+              <input type="date" id="wd-due-date" value="2024-05-13" />
+              <br /><br />
+              <table>
+                <tr>
+                  <td align="left">
+                  Available from<br />
+                  <input type="date" id="wd-available-from" value="2024-05-06" />
+                  </td>
+                  <td>
+                  Until<br />
+                  <input type="date" id="wd-available-until" value="2024-05-20" />
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          {/* <tr>
+            <td>
+            </td>
+            <td>
+              <button>Cancel</button> <button>Save</button>
+            </td>
+          </tr> */}
+        </table>
+        <hr />
+        <button>Cancel</button> <button>Save</button>
+        
+                
+      </div>
+  );
+  }
+  
---
+++ src/Labs/index.tsx
@@ -0,0 +1,27 @@
+import Lab1 from "./Lab1";
+import { Route, Routes, Navigate } from "react-router";
+import TOC from "./TOC";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+export default function Labs() {
+  return (
+    <div>
+      <h1>Labs</h1>
+      My name: Rebecca Choi
+      <TOC />
+      <Routes>
+        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="Lab1" element={<Lab1 />} />
+        <Route path="Lab2" element={<Lab2 />} />
+        <Route path="Lab3" element={<Lab3 />} />
+      </Routes>
+    </div>
+  );
+  <div id="wd-p-tag">
+  <h4>Paragraph Tag</h4>
+    <p id="wd-p-1">
+This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
+  </p>
+</div>
+
+}
