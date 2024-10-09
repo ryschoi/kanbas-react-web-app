1ed98ad1ec2a6be027b54bb75047c782d9f5a88c
ryschoi <148027367+ryschoi@users.noreply.github.com>
2024-10-08 20:07:08 -0400

    Add files via upload

---
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -0,0 +1,37 @@
+// import MdDoNotDisturbAlt from "./MdDoNotDisturbAlt"
+// import FaCheckCircle from "./FaCheckCircle"
+// import BiImport from "./BiImport"
+// import LiaFileImportSolid from "./LiaFileImportSolid";
+
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+export default function CourseStatus() {
+    return (
+      <div id="wd-course-status">
+        <div id="wd-course-status" style={{ width: "300px" }}></div>
+        <h2>Course Status</h2>
+        <div className="d-flex">
+        <div className="w-50 pe-1">
+        <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </button>
+        </div>
+        <div className="w-50">
+          <button className="btn btn-lg btn-success w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish </button>
+        </div>
+      </div><br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" /> Import Existing Content </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </button>
+
+        <button>Choose Home Page</button> <br />
+        <button>View Course Stream</button> <br />
+        <button>New Announcement</button> <br />
+        <button>New Analytics</button> <br />
+        <button>View Course Notifications</button>
+      </div>
+  );}
+  
---
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -0,0 +1,7 @@
+import { BsGripVertical } from "react-icons/bs";
+export default function ModuleControlButtons() {
+  return (
+    <div className="float-end">
+      <BsGripVertical />
+    </div>
+);}
