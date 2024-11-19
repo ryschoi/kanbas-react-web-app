eac9937d78e529848a819fee53a169693a2b9fa6
ryschoi <choi.re@northeastern.edu>
2024-11-19 14:30:34 -0500

    Resetting A4 - delete old src

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++
@@ -1,7 +0,0 @@
-import { BsGripVertical } from "react-icons/bs";
-export default function ModuleControlButtons() {
-  return (
-    <div className="float-end">
-      <BsGripVertical />
-    </div>
-);}
